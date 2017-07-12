946bd8b071cb7a215af6b2d556a141c8a75f220c
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-12 07:53:40 +0200

    Placeholder description about the Whatpulse stats

--- src/components/Whatpulse/Index.vue
+++ src/components/Whatpulse/Index.vue
@@ -4,14 +4,18 @@
       .row.py-5.text-center
         whatpulse-stat(type="keys")
         whatpulse-stat(type="clicks")
+      whatpulse-text
+        p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent convallis turpis quis nulla lobortis tincidunt. Phasellus aliquam odio elit, nec pretium velit dapibus sit amet. Curabitur nec vestibulum lectus. Donec laoreet porta nisl in lobortis. Ut nec metus velit. Sed nec elementum tortor. Quisque ac leo rutrum, congue lectus vitae, rutrum nibh. Donec faucibus elit eu iaculis blandit.
 </template>
 
 <script>
+  import Text from '@/components/Helpers/Text.vue'
   import Stat from '@/components/Whatpulse/Stat.vue'
 
   export default {
     name: 'portfolio-whatpulse',
     components: {
+      whatpulseText: Text,
       whatpulseStat: Stat
     }
   }
