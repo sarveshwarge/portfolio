e772073b9bd3bc32ae7d15626c2f5d99895fde77
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-16 23:56:31 +0200

    Started with the skills section

---
+++ src/components/Skills/Group.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .col-4
+    .row
+      .col-12
+        strong.text-uppercase.midnight-blue(v-html="skill.name")
+    .row
+      .col-12.mt-2
+        keyword(v-for="(keyword, index) in keywords",
+                :keyword="keyword")
+</template>
+
+<script>
+  import Keyword from '@/components/Skills/Keyword'
+
+  export default {
+    name: 'skill-group',
+    props: {
+      skill: {
+        required: true,
+        type: Object
+      }
+    },
+    components: {
+      keyword: Keyword
+    },
+    computed: {
+      keywords () {
+        return this.skill.keywords
+      }
+    }
+  }
+</script>
---
+++ src/components/Skills/Keyword.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  span.d-block
+    i.fa.fa-check-circle-o.mr-2.midnight-blue
+    span.asbestos.fw2(v-html="keyword")
+</template>
+
+<script>
+  export default {
+    name: 'skill-keyword',
+    props: {
+      keyword: {
+        required: true,
+        type: String
+      }
+    }
+  }
+</script>
