a7a10e5cc9882286b98a1cf669e5b231fba73914
Maarten Paauw <maartenpaauw@gmail.com>
2017-07-16 17:31:27 +0200

    Started with the references section

---
+++ src/components/References/Control.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .col-2.text-center
+    i.fa.fa-3x.midnight-blue(:class="icon")
+</template>
+
+<script>
+  export default {
+    name: 'reference-control',
+    props: {
+      direction: {
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      icon () {
+        return `fa-angle-${this.direction}`
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped="scoped">
+  i {
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/References/Indicators.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .col-12.text-center
+    i.fa.mr-2.midnight-blue(v-for="(reference, index) in references",
+                            :class="icon(index)"
+                            :key="index")
+</template>
+
+<script>
+  export default {
+    name: 'references-indicators',
+    props: {
+      references: {
+        required: true,
+        type: Array
+      },
+      current: {
+        required: true,
+        type: Number
+      }
+    },
+    methods: {
+      icon (index) {
+        return index === this.current ? 'fa-dot-circle-o' : 'fa-circle-o'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped="scoped">
+  i {
+    cursor: pointer;
+  }
+</style>
