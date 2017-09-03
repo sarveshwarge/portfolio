e5bb2dfd127f462d2571f65d19f73c10bffdfdf3
Maarten Paauw <maartenpaauw@gmail.com>
2017-09-03 12:44:40 +0200

    chore(footer): id in camel case

--- src/components/Footer/Social.vue
+++ src/components/Footer/Social.vue
@@ -3,17 +3,18 @@
     .col.text-center
       span(v-for="(profile, index) in profiles",
            :key="index")
-        b-tooltip(:target="profile.network.toLowerCase()",
+        b-tooltip(:target="id(profile.network)",
                   :title="profile.network")
         a.mr-2.concrete.hover-clouds(:href="profile.url",
                                      target="_blank",
                                      rel="noopener",
-                                     :id="profile.network.toLowerCase()")
+                                     :id="id(profile.network)")
           i.fa.fa-lg(:class="icon(profile.network)")
 </template>
 
 <script>
   import slugify from 'slugify'
+  import camelcase from 'camelcase'
   import { mapGetters } from 'vuex'
 
   export default {
@@ -24,6 +25,9 @@
       ])
     },
     methods: {
+      id (network) {
+        return camelcase(network)
+      },
       icon (network) {
         return `fa-${slugify(network).toLowerCase()}`
       }
