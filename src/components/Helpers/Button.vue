0234b7e7a1b92e5698e561713bfd818a2e7235fa
Maarten Paauw <maartenpaauw@gmail.com>
2017-09-03 11:56:17 +0200

    chore(download): style download cv button

--- src/components/Helpers/Button.vue
+++ src/components/Helpers/Button.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  b-button.text-uppercase(:variant="variant",
+  b-button.text-uppercase(:variant="classes",
                           :href="href")
     slot
 </template>
@@ -15,8 +15,29 @@
       variant: {
         required: false,
         value: String,
-        default: 'outline-secondary'
+        default: 'dark'
+      }
+    },
+    data () {
+      return {
+        variants: {
+          dark: 'btn-outline-secondary bg-transparent border-asbestos asbestos hover-white border-hover-white',
+          light: 'btn-secondary bg-midnight-blue border-midnight-blue white hover-midnight-blue bg-hover-transparent'
+        }
+      }
+    },
+    computed: {
+      classes () {
+        return this.variants[this.variant]
       }
     }
   }
 </script>
+
+<style lang="scss" scoped="scoped">
+  .bg-hover-transparent {
+    &:hover {
+      background-color: transparent;
+    }
+  }
+</style>
